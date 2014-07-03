34a178b8635181ba3c27eb5eeb66e989d8c1d405
lecorr_b <thomas.lecorre@epitech.eu>
2014-07-03 18:29:47 +0200

    actions.py decharger un peu le algo.py

---
+++ IA/actions.py
@@ -0,0 +1,18 @@
+import os, sys
+import socket
+from iaClass import *
+from algo import *
+
+def callOthers(ia):
+	nbPlayers = 1
+	i = 0
+	msg = "stop " + str(ia.lvl)
+	while nbPlayers != ia.listNbPlayers[ia.lvl - 1]:
+		for fullCase in ia.listVoir:
+			splitCase = fullCase.split(' ')
+			for item in splitCase:
+				if (i == 0):
+					if (item == "joueur"):
+						nbPlayers += 1
+			i += 1
+	ia.broadcast(msg)
