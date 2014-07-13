36ff593efb07cc20cb80bba74966aa32a80843e9
lecorr_b <thomas.lecorre@epitech.eu>
2014-07-13 16:41:54 +0200

    ooo

--- serveur/src/serveur/iaManagement.c
+++ serveur/src/serveur/iaManagement.c
@@ -11,29 +11,32 @@ static void	sendingIAData(serveur *this, iaClients* ia, teams* team) {
   pushNode(ia->wrBuffer, sending);
 }
 
-static int	push_in_waiting(serveur* this, wclients* node) {
+static void	push_in_waiting(serveur* this, wclients* node) {
   teams*	team;
   iaClients*	ia;
 
-  if (!node)
-    return (0);
-  team = getTeamById(this, node->team);
-  if (!team->unaff_size)
-    return (push_in_waiting(this, (wclients*)node->_.next));
-  ia = team->list;
-  while (ia && ia->iaClient != -1 && (ia = ia->next) != NULL);
-  if (!ia)
-    return (push_in_waiting(this, node));
-  ia->iaClient = node->_.client;
-  ia->rdBuffer = node->_.rdBuffer;
-  ia->state = (ia->state == unaffected) ? (alive) : (ia->state);
-  team->unaff_size -= 1;
-  sendingIAData(this, ia, team);
-  if (ia->state == egg)
-    avertMonitor(this, mConnectEgg(ia->num));
-  avertMonitor(this, mNewPlayer(ia->num, ia->_p.x, ia->_p.y, ia->_o,
-				ia->lvl, node->team));
-  return (push_in_waiting(this, del_waiting(this, node, false)));
+  while (node)
+    {
+      team = getTeamById(this, node->team);
+      ia = team->list;
+      while (ia && ia->iaClient != -1 && (ia = ia->next) != NULL);
+      if (!ia)
+	node = (wclients*)node->_.next;
+      else
+	{
+	  ia->iaClient = node->_.client;
+	  ia->rdBuffer = node->_.rdBuffer;
+	  ia->state = (ia->state == unaffected) ? (alive) : (ia->state);
+	  team->unaff_size -= 1;
+	  sendingIAData(this, ia, team);
+	  if (ia->state == egg)
+	    avertMonitor(this, mConnectEgg(ia->num));
+	  avertMonitor(this, mNewPlayer(ia->num, ia->_p.x, ia->_p.y, ia->_o,
+					ia->lvl, node->team));
+	  node = del_waiting(this, node, false);
+	}
+    }
+
 }
 
 void	actualize_IA(serveur* this, fd_set* rd, fd_set* wr, bool buffered) {
